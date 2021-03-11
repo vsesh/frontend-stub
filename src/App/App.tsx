37ee2d118dc75ca26d63d3e1ef5b67ef603be8f5
Vsevolod Shmyroff <vsesh@yandex-team.ru>
2021-03-11 17:33:13 +0300

    setup webpack css-loader

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,14 @@
 import React, {PureComponent} from 'react';
+import {block} from 'bem-cn';
+
+import './App.css';
+
+const b = block('App');
 
 class App extends PureComponent {
     render() {
         return (
-            <div>
+            <div className={b()}>
                 test
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,12 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
             }
-        ],
+        ]
     },
     output: {
         filename: '[name].js',
