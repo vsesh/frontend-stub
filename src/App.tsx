acd0ab012f477ca8246407e89dfb7ece4ffa846b
Vsevolod Shmyroff <vsesh@yandex-team.ru>
2021-03-11 13:55:55 +0300

    ts && webpack setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+class App extends PureComponent {
+    render() {
+        return (
+            <div>
+                test
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const OUTPUT_PATH = path.resolve(__dirname, 'build');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: OUTPUT_PATH
+    },
+    devServer: {
+        contentBase: OUTPUT_PATH,
+        compress: true,
+        open: true,
+        hot: true,
+        port: 9000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/templates', 'index.html')
+        })
+    ]
+}
